13cd5f8aa6bef715c5f9b96e3ac8429d8633038b
fangan <fangan@songxiaocai.com>
2019-10-12 19:52:37 +0800

    trace

--- traceParent/traceAgent/src/main/java/lljk/xtrace/boot/AgentTransformerWatch.java
+++ traceParent/traceAgent/src/main/java/lljk/xtrace/boot/AgentTransformerWatch.java
@@ -1,9 +1,15 @@
 package lljk.xtrace.boot;
 
 import lljk.xtrace.traceOption.constant.AgentDelegateClassProfile;
+import lljk.xtrace.traceUtil.propertiesUtil.PropertiesFactory;
+import lljk.xtrace.traceUtil.propertiesUtil.PropertiesFileEnum;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
 
-import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 import java.security.ProtectionDomain;
+import java.util.Properties;
 
 /**
  * @Description TODO
@@ -40,15 +46,39 @@ public class AgentTransformerWatch {
 
     public byte[] onStart(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer,ClassLoader agentLoader){
         try {
-            AgentStart agentStart = new AgentStart();
-            return agentStart.transform(loader,className,classBeingRedefined,protectionDomain,classfileBuffer,agentLoader);
-//            Class clazz =  agentLoader.loadClass("lljk.xtrace.boot.AgentStart");
-//            Object o = clazz.newInstance();
-//            Method method = clazz.getMethod("transform",ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class ,ClassLoader.class);
-//            return (byte[])method.invoke(o,loader,className,classBeingRedefined,protectionDomain,classfileBuffer,agentLoader);
+            return transform(loader,className,classBeingRedefined,protectionDomain,classfileBuffer,agentLoader);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer, ClassLoader agentClassLoader){
+
+        return handle(loader,className,classBeingRedefined,protectionDomain,classfileBuffer,agentClassLoader);
+
+
+    }
+
+
+    public byte[] handle(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer, ClassLoader agentClassLoader) {
+        byte[] originByte = classfileBuffer;
+        try {
+            PropertiesFactory propertiesFactory = new PropertiesFactory();
+            Properties properties = propertiesFactory.getProperties(PropertiesFileEnum.CLASSVISITOR);
+            String classVisitorClassName = properties.getProperty(className);
+            ClassReader cr = new ClassReader(classfileBuffer);
+            ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+            Class clazz =  this.getClass().getClassLoader().loadClass(classVisitorClassName);
+            Constructor<?> cons = clazz.getConstructor(ClassVisitor.class);
+            ClassVisitor findClassVisitor = (ClassVisitor)cons.newInstance(cw);
+            cr.accept(findClassVisitor,0);
+            return cw.toByteArray();
+        }catch (Exception e){
+            System.err.print("DefaultDispatcherTransform#transform异常"+e.getMessage());
+            return originByte;
+        }
+
     }
 }
--- traceParent/traceOption/src/main/java/lljk/xtrace/traceOption/dispatcher/DispatcherTransform.java
+++
@@ -1,8 +0,0 @@
-package lljk.xtrace.traceOption.dispatcher;
-
-import java.security.ProtectionDomain;
-
-public interface DispatcherTransform {
-    byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer, ClassLoader agentClassLoader);
-
-}
